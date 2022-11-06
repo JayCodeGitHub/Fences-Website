c2ce38c8844d2fddfb2b1be4f8595b36b6264d4a
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-06 23:18:37 +0100

    Styled ContactItemsSection

--- src/assets/styles/pages/kontakt.styles.js
+++ src/assets/styles/pages/kontakt.styles.js
@@ -1,6 +1,65 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-    height: 80rem;
-    padding-top: 10rem;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 12rem;
+    ${({ theme }) => theme.mq.bigDesktop} {
+        padding: 0 12vw 2rem 12vw;
+        min-height: 60vh;
+    }
 `
+export const ContactItemsWrapper = styled.div`
+    width: 50%;
+    padding: 0 5% 0 5%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+`
+
+export const ContactItem = styled.span`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    margin: 3rem 0;
+    padding-left: 5rem;
+    position: relative;
+   p {
+        margin: 0 0 0 15rem;
+        width: 20rem;
+        text-align: center;
+        font-size: ${({ theme }) => theme.font.size.s};
+    }  
+    svg {
+        height: 50px;
+        stroke: ${({ theme }) => theme.primary};   
+    }
+    &::before {
+        content: '';
+        width: 120%;
+        height: 4px;
+        border-radius: 5px;
+        background-color: ${({ theme }) => theme.grey};
+        position: absolute;
+        top: 125%;
+        left: -10%;
+    }
+`
+
+export const CompanyData = styled.div`
+    p {
+        font-size: ${({ theme }) => theme.font.size.s};
+    }
+`
+
+export const MapWrapper = styled.div`
+    width: 40%;
+    padding: 2rem;
+`
+
+export const StyledMap = styled.iframe`
+    width: 100%;
+    height: 100%;
+`;
--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -1,12 +1,46 @@
 import * as React from 'react';
 import { MainTemplate } from '../templates/MainTemplate';
-import { Wrapper } from '../assets/styles/pages/kontakt.styles';
+import {
+  Wrapper,
+  ContactItemsWrapper,
+  ContactItem,
+  CompanyData,
+  MapWrapper,
+  StyledMap,
+} from '../assets/styles/pages/kontakt.styles';
+import PhoneIcon from '../assets/icons/PhoneIcon.svg';
+import MailIcon from '../assets/icons/MailIcon.svg';
 
 function KontaktPage() {
   return (
     <MainTemplate>
       <Wrapper>
-        <h1>Kontakt</h1>
+        <ContactItemsWrapper>
+          <ContactItem>
+            <PhoneIcon />
+            <p>999 888 333</p>
+          </ContactItem>
+          <ContactItem>
+            <MailIcon />
+            <p>kontakt@gmail.com</p>
+          </ContactItem>
+          <CompanyData>
+            <p>Nazwa Firmy</p>
+            <p>Adres</p>
+            <p>Kod Pocztowy</p>
+            <p>NIP 00000000000</p>
+            <p>REGON 0000000</p>
+          </CompanyData>
+        </ContactItemsWrapper>
+        <MapWrapper>
+          <StyledMap
+            title="mapa"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2420.984138782941!2d16.81528761613564!3d52.64219957983751!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47046cb8d617b447%3A0xb0787118b66f1468!2sGo%C5%82aszy%C5%84ska%2035%2C%2064-600%20Oborniki!5e0!3m2!1spl!2spl!4v1660782076893!5m2!1spl!2spl"
+            allowFullScreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></StyledMap>
+        </MapWrapper>
       </Wrapper>
     </MainTemplate>
   );
