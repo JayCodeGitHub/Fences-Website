a1c21ef6dfee8806b0c8955012af3a39d58c0b73
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-07 23:56:49 +0100

    Styled Realizacje Page

--- src/assets/styles/pages/realizacje.styles.js
+++ src/assets/styles/pages/realizacje.styles.js
@@ -2,4 +2,29 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     min-height: 100vh;
+    padding: 5vw;
+    ${({ theme }) => theme.mq.desktop} {
+        padding: 2.5vw 5vw 2.5vw 5vw;
+        display: grid;
+        width: 100%;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 50px;
+    }
+    ${({ theme }) => theme.mq.bigDesktop} {
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+`
+
+export const StyledImage = styled.img`
+    width: 100%;
+    margin: 1rem 0;
+    border-radius: 5px;
+    ${({ theme }) => theme.mq.desktop} {
+        height: 28vw;
+        object-fit: cover;
+    }
+    ${({ theme }) => theme.mq.bigDesktop} {
+        height: 18vw;
+    }
 `
--- src/pages/Realizacje.jsx
+++ src/pages/Realizacje.jsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
 import { MainTemplate } from '../templates/MainTemplate';
 import Header from '../components/Header/Header';
-import { Wrapper } from '../assets/styles/pages/realizacje.styles';
+import { Wrapper, StyledImage } from '../assets/styles/pages/realizacje.styles';
+import One from '../assets/images/1.jpg';
+import Two from '../assets/images/2.jpg';
+import Three from '../assets/images/3.jpeg';
+import Four from '../assets/images/4.jpeg';
+import Five from '../assets/images/5.jpeg';
+import Six from '../assets/images/6.jpeg';
+import Seven from '../assets/images/7.jpeg';
+import Eight from '../assets/images/8.jpeg';
 
 function RealizacjePage() {
   return (
     <MainTemplate>
       <Header title="Realizacje" />
-      <Wrapper>gallery</Wrapper>
+      <Wrapper>
+        <StyledImage src={One} />
+        <StyledImage src={Two} />
+        <StyledImage src={Three} />
+        <StyledImage src={Four} />
+        <StyledImage src={Five} />
+        <StyledImage src={Six} />
+        <StyledImage src={Seven} />
+        <StyledImage src={Eight} />
+      </Wrapper>
     </MainTemplate>
   );
 }
