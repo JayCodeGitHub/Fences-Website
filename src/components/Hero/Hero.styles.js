653b179603bd5f6cf8ea46c53ef95826cb38b02d
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-07 19:45:35 +0100

    Styled Hro primary for Mobile

--- src/components/Hero/Hero.styles.js
+++ src/components/Hero/Hero.styles.js
@@ -3,74 +3,95 @@ import styled, {css} from "styled-components";
 export const Wrapper = styled.div`
     background-color: ${({ theme }) => theme.primary};
     width: 100%;
-    margin-top: 8rem;
-    z-index: -10;
+    margin: 8rem 0 15rem 0;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+    height: 50rem;
     ${({ theme }) => theme.mq.desktop} {
+        position: relative;
         margin: 11.5rem 0 15rem 0;
         height: 60rem;
     }
     ${({secondary}) => (
         secondary && css`
-            margin: 0 0 15rem 0;
             background-image: url(${({ background }) => background});
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
-            position: relative;
             ${({ theme }) => theme.mq.desktop} {
-                margin-top: 0;
-                height: 60rem;
+                margin: 0 0 15rem 0;    
             }
         `
     )}
 `
 
 export const StyledHeader = styled.header`
-    width: 28%;
-    position: absolute;
-    top: 30%;
-    left: 6%;
+    width: 80%;
+    padding: 3rem 0; 
     h1 {
         margin: 0;
         width: 100%;
         color: ${({ theme }) => theme.white};
-        font-size: ${({ theme }) => theme.font.size.l};
+        font-size: ${({ theme }) => theme.font.size.m};
     }
     button {
         border: none;
-        background-color: ${({ theme }) => theme.grey};
         padding: 1rem;
-        font-size: ${({ theme }) => theme.font.size.s};
-        margin: 4rem 2rem;
+        margin: 2rem 1rem;
+        background-color: ${({ theme }) => theme.grey};
+        font-size: ${({ theme }) => theme.font.size.xs};
     }
     a {
         text-decoration: none;
         color: inherit;
     }
+    ${({ theme }) => theme.mq.desktop} {
+        position: absolute;
+        width: 28%;
+        top: 30%;
+        left: 6%;
+        h1 {
+            font-size: ${({ theme }) => theme.font.size.l};
+        }
+        button {
+            font-size: ${({ theme }) => theme.font.size.s};
+            margin: 4rem 2rem;
+        }
+    }
     ${({secondary}) => (
         secondary && css`
-            background-color: ${({ theme }) => theme.primary};
-            width: 65rem;
-            padding: 4rem 4rem 10rem 4rem;
-            position: absolute;
-            top: 30%;
             left: 55%;
+            width: 65rem;
+            position: relative;
+            padding: 4rem 4rem 10rem 4rem;
+            background-color: ${({ theme }) => theme.primary};
             h1 {
                 font-size: ${({ theme }) => theme.font.size.m};
-                color: ${({ theme }) => theme.white};
             }
             p {
                 font-size: ${({ theme }) => theme.font.size.m};
                 color: ${({ theme }) => theme.white};
+            }
+            ${({ theme }) => theme.mq.desktop} {
+                left: 55%;
             }
         `
     )}
 `
 
 export const StyledImage = styled.img`
-width: 50%;
+    width: 80%;
+    height: 40rem;
+    position: absolute;
+    object-fit: cover;
+    top: 35%;
+    ${({ theme }) => theme.mq.desktop} {
+        width: 50%;
+        height: auto;
         position: absolute;
         top: 20%;
         left: 40%;
-        z-index: 0;
+    }
 `
