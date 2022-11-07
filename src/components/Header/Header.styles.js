4b384f9a459423e14801944441814f83acc8f58a
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-07 01:11:15 +0100

    Styled Header for Mobile

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.header`
     width: 100%;
-    margin-top: 11rem;
-    height: 18rem;
+    margin-top: 8rem;
+    height: 15rem;
     background-color: ${({ theme }) => theme.lightGrey};
     display: flex;
     justify-content: center;
@@ -11,7 +11,14 @@ export const Wrapper = styled.header`
     h1 {
         margin: 0;
         padding: 0;
-        font-size: ${({ theme }) => theme.font.size.l};
+        font-size: ${({ theme }) => theme.font.size.m};
         color: ${({ theme }) => theme.black};
     }
+    ${({ theme }) => theme.mq.desktop} {
+    margin-top: 11rem;
+    height: 18rem;
+        h1 {
+        font-size: ${({ theme }) => theme.font.size.l};
+    }
+}
 `
