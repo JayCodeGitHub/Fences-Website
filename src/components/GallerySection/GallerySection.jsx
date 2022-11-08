c2646c48769c1c5b28ef9f9b7126a4d6bc234d53
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-08 20:55:29 +0100

    Created GallerySection

---
+++ src/components/GallerySection/GallerySection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './GallerySection.styles';
+
+const GallerySection = () => {
+  return (
+    <Wrapper>
+      <h1>Gallery Section</h1>
+    </Wrapper>
+  );
+};
+
+export default GallerySection;
