5ad7c8411357e7066530f11df40bea5555ea4ba9
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-11 12:58:59 +0100

    Created GridSection

---
+++ src/components/GridSection/GridSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './GridSection.styles';
+
+const GridSection = () => {
+  return (
+    <Wrapper>
+      <h1>GridView</h1>
+    </Wrapper>
+  );
+};
+
+export default GridSection;
