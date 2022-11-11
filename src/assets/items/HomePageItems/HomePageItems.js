3c8aaee29077fb45aca32068b9632363f3338c54
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-11 02:34:20 +0100

    Refactored Hamepage with .map

---
+++ src/assets/items/HomePageItems/HomePageItems.js
@@ -0,0 +1,74 @@
+import Three from '../../images/3.jpeg'
+import Four from '../../images/4.jpeg';
+import Five from '../../images/5.jpeg';
+import Six from '../../images/6.jpeg';
+import Seven from '../../images/7.jpeg';
+import Eight from '../../images/8.jpeg';
+
+export const OffersSectionItems = [
+    {
+        title: "Ogrodzenia Nowoczesne",
+        image: Three,
+    },
+    {
+        title: "Ogrodzenia Klasyczne",
+        image: Four,
+    },
+    {
+        title: "Ogrodzenia Murowane",
+        image: Five,
+    },
+    {
+        title: "Balustrady",
+        image: Six,
+    },
+    {
+        title: "Automatyka Bramowa",
+        image: Seven,
+    },
+    {
+        title: "Prace Murarskie",
+        image: Eight,
+    },
+]
+
+export const StepsSectionItems = [
+    {
+        number: "1.",
+        heading: "lorem ipsum dolor sit amet",
+        paragraph: " Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th",
+    },
+    {
+        number: "2.",
+        heading: "lorem ipsum dolor sit amet",
+        paragraph: " Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th",
+    },
+    {
+        number: "3.",
+        heading: "lorem ipsum dolor sit amet",
+        paragraph: " Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th",
+    },
+    {
+        number: "4.",
+        heading: "lorem ipsum dolor sit amet",
+        paragraph: " Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th",
+    },
+]
+
+export const GallerySectionItems = [
+    {
+        image: Three,
+    },
+    {
+        image: Four,
+    },
+    {
+        image: Five,
+    },
+    {
+        image: Six,
+    },
+    {
+        image: Seven,
+    },
+]
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,6 @@ import { MainTemplate } from '../templates/MainTemplate';
 import ParagraphBar from '../components/ParagraphBar/ParagraphBar';
 import Hero from '../components/Hero/Hero';
 import Photo from '../assets/images/1.jpg';
-import PhotoTwo from '../assets/images/4.jpeg';
 import {
   Title,
   Paragraph,
@@ -12,6 +11,11 @@ import {
   StepsSection,
   GallerySection,
 } from '../assets/styles/pages/homepage.styles';
+import {
+  OffersSectionItems,
+  GallerySectionItems,
+  StepsSectionItems,
+} from '../assets/items/HomePageItems/HomePageItems';
 
 function IndexPage() {
   return (
@@ -33,82 +37,30 @@ function IndexPage() {
       <OffersSection>
         <h1>Nasza Oferta</h1>
         <div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
+          {OffersSectionItems.map(({ image }) => (
+            <div>
+              <img src={image} />
+            </div>
+          ))}
         </div>
       </OffersSection>
       <StepsSection>
-        <div>
-          <h3>1.</h3>
-          <h4>lorem ipsum dolor sit amet</h4>
-          <p>
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th
-          </p>
-        </div>
-        <div>
-          <h3>2.</h3>
-          <h4>lorem ipsum dolor sit amet</h4>
-          <p>
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th
-          </p>
-        </div>
-        <div>
-          <h3>3.</h3>
-          <h4>lorem ipsum dolor sit amet</h4>
-          <p>
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th
-          </p>
-        </div>
-        <div>
-          <h3>4.</h3>
-          <h4>lorem ipsum dolor sit amet</h4>
-          <p>
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th
-          </p>
-        </div>
+        {StepsSectionItems.map(({ number, heading, paragraph }) => (
+          <div>
+            <h3>{number}</h3>
+            <h4>{heading}</h4>
+            <p>{paragraph}</p>
+          </div>
+        ))}
       </StepsSection>
       <GallerySection>
         <h1>Nasze Realizacje</h1>
         <div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
-          <div>
-            <img src={PhotoTwo} />
-          </div>
+          {GallerySectionItems.map(({ image }) => (
+            <div>
+              <img src={image} />
+            </div>
+          ))}
           <div>
             <Link to="/Realizacje">Zobacz Wszystkie Realizacje</Link>
           </div>
