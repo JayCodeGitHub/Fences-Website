22b8b40139e8b7632c752da6f986ace2db682a2e
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-16 01:05:01 +0100

    Conected with DatoCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
+const path = require('path');
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: `Fences-Website`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: process.env.API_DATO_CMS,
+      },
+    },
   {
     resolve: 'gatsby-plugin-google-fonts',
     options: {
