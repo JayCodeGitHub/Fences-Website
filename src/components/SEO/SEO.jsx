047930836dc51350c88322ca1c2a9c12751e323e
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-11-18 18:03:54 +0100

    Created SEO component

---
+++ src/components/SEO/SEO.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+const SEO = () => (
+  <Helmet>
+    <html lang="pl" />
+    <meta name="description" content="test" />
+  </Helmet>
+);
+
+export default SEO;
