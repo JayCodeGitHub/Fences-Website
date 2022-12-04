3df64f96afa8ff2c46ead7d42afcf8c651c2c897
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-04 22:40:45 +0100

    Created ManufacturersSection

---
+++ src/components/ManufacturersSection/ManufacturersSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './ManufacturersSection.styles';
+
+const ManufacturersSection = () => {
+  return (
+    <Wrapper>
+      <h1>ManufacturersSection</h1>
+    </Wrapper>
+  );
+};
+
+export default ManufacturersSection;
