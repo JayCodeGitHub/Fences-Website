2e9a5c6b32c2decaf343539c6b3a7b87f047834a
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-06 16:00:20 +0100

    Created IconsSection

---
+++ src/components/IconsSection/IconsSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from './IconsSection.styles';
+
+const IconsSection = () => {
+  return (
+    <Wrapper>
+      <h1>IconsSection</h1>
+    </Wrapper>
+  );
+};
+
+export default IconsSection;
